9b973c48d56802db26e8fe862746e54aba77f63b
Manunson <manunson@mail.bradley.edu>
2023-10-02 09:57:46 -0500

    Adding an intro

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,4 +1,5 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
+import '../styles/components/intro.css'
 import '../styles/utils.css'
